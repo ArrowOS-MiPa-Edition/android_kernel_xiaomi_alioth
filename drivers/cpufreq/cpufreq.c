98a4a6cbb92642645f74bafe9b351cd9ab9cac21
palaych <palaych@arrowos.net>
2023-03-27 04:56:42 +0900

    drivers: thermal: Revert xiaomi modifications
     * Will re-impl.
    
    Squash of:
    
    Author: palaych <palaych@arrowos.net>
    Date:   Mon Mar 27 04:56:18 2023 +0900
    
        Revert "thermal: core: Implement thermal config switch"
    
        This reverts commit 2ae23d2754728d78f5f92a1574dca6babf4eb11c.
    
    Author: palaych <palaych@arrowos.net>
    Date:   Mon Mar 27 04:56:16 2023 +0900
    
        Revert "thermal: bcl_pmic5: Apply xiaomi thermal modifications"
    
        This reverts commit a71324b3065ce72db36240aeda18ef2a480b1a53.
    
    Author: palaych <palaych@arrowos.net>
    Date:   Mon Mar 27 04:56:14 2023 +0900
    
        Revert "thermal: qcom: bcl: Return before thermal trip evaluates"
    
        This reverts commit 8aed027f99207c45496dd6b438f6220a6b93a807.
    
    Author: palaych <palaych@arrowos.net>
    Date:   Mon Mar 27 04:56:12 2023 +0900
    
        Revert "thermal: Add support for new xiaomi thermal message nodes"
    
        This reverts commit 7d46cbccb4b173dc5caf8dc1cd9fa9906bcbe9d9.
    
    Author: palaych <palaych@arrowos.net>
    Date:   Mon Mar 27 04:56:10 2023 +0900
    
        Revert "thermal: cpu_cooling: Fix cpu limits set current frequency"
    
        This reverts commit 6ed226006783915825634f4de6e175132a8f71eb.
    
    Change-Id: I97a6669aa4396ae4f408b29bb7c7a3ec6cf8df18

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2266,9 +2266,9 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_ADJUST, new_policy);
 
-	/* the adjusted frequency should not exceed thermal limit*/
+	/* adjust if necessary - hardware incompatibility */
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
-			CPUFREQ_THERMAL, new_policy);
+			CPUFREQ_INCOMPATIBLE, new_policy);
 
 	/*
 	 * verify the cpu speed can be set within this limit, which might be
